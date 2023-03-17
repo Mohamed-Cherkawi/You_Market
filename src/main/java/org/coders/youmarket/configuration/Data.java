51507818bf8ff0bde1f6939aab78328e80293ece
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-17 16:11:20 +0100

    Removed useless code and refactored

--- src/main/java/org/coders/youmarket/configuration/Data.java
+++ src/main/java/org/coders/youmarket/configuration/Data.java
@@ -2,10 +2,7 @@ package org.coders.youmarket.configuration;
 
 import lombok.RequiredArgsConstructor;
 import org.coders.youmarket.entities.AppUser;
-import org.coders.youmarket.entities.Role;
 import org.coders.youmarket.enums.AvailabilityStateEnum;
-import org.coders.youmarket.enums.RoleEnum;
-import org.coders.youmarket.repositories.RoleRepository;
 import org.coders.youmarket.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -13,25 +10,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 
 @Configuration @RequiredArgsConstructor
 public class Data {
-    private final RoleRepository roleRepository;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Bean
     @Transactional
     public CommandLineRunner insertSomeData() {
+        AppUser user = AppUser.builder().
+                reference(UUID.randomUUID().toString()).
+                username("Simox").
+                password(passwordEncoder.encode("pass123")).
+                name("Mohamed").
+                status(AvailabilityStateEnum.ONLINE).
+                phone("0618387383").
+                createdAt(LocalDateTime.now()).build();
 
-        return args -> {
-            Role clientRole = new Role(RoleEnum.CLIENT);
-            roleRepository.saveAll(List.of(new Role(RoleEnum.ADMIN), clientRole));
-
-            userRepository.save(new AppUser(null , UUID.randomUUID().toString(),"Simox", passwordEncoder.encode("pass123"),"Mohamed","0618387383","EE03228", AvailabilityStateEnum.ONLINE ,clientRole));
-        };
+        return args -> userRepository.save(user);
     }
 }
