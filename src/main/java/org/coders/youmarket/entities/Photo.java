b1ee0c79e21742fe6c295586e72d6d26243fcb14
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-18 19:25:22 +0100

    Create the address entity

---
+++ src/main/java/org/coders/youmarket/entities/Photo.java
@@ -0,0 +1,26 @@
+package org.coders.youmarket.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "photo")
+public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "photo_seq")
+    @SequenceGenerator(name = "photo_seq")
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+}
