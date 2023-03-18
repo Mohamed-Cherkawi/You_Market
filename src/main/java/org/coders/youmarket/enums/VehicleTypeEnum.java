c7eee7da0079c4fe6d4232547f7942c569d9dd1d
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-18 19:26:27 +0100

    Create the VehicleTypeEnum that groups types of vehicles

---
+++ src/main/java/org/coders/youmarket/enums/VehicleTypeEnum.java
@@ -0,0 +1,11 @@
+package org.coders.youmarket.enums;
+
+public enum VehicleTypeEnum {
+    CAR_TRACK ,
+    MOTORCYCLE,
+    POWERSPORT, // Quad
+    RV_CAMPER,
+    TRAILER, // Tied vehicle
+    BOAT,
+    COMMERCIAL_INDUSTRIAL
+}
