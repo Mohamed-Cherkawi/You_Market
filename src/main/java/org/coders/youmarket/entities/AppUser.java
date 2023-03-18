ea1b673c8f4282c586a42a113d554b5c9d64a5c6
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-18 19:22:59 +0100

    Add some modifications , add some fields and removed some

--- src/main/java/org/coders/youmarket/entities/AppUser.java
+++ src/main/java/org/coders/youmarket/entities/AppUser.java
@@ -24,6 +24,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.Collections;
 
 
 @Entity @Builder @Getter @Setter @ToString @NoArgsConstructor @AllArgsConstructor
@@ -72,7 +73,7 @@ public class AppUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.emptyList();
     }
 
     @Override
