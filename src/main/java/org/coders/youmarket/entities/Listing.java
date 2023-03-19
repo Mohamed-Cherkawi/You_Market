21ea4f38a1b90f4d3786209da43657eced53b706
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-19 18:21:01 +0000

    Add the SuperBuilder annotations to prevent the boiler plate code

--- src/main/java/org/coders/youmarket/entities/Listing.java
+++ src/main/java/org/coders/youmarket/entities/Listing.java
@@ -11,34 +11,30 @@ import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Entity @Inheritance(strategy = InheritanceType.JOINED)
-@AllArgsConstructor @NoArgsConstructor @Builder
+@AllArgsConstructor @NoArgsConstructor @SuperBuilder
 @Getter @Setter @ToString
 public class Listing {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, unique = true)
     private Long id;
+
     private String ownerReference;
 
-    @ToString.Exclude
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "listing_id")
     private Set<Photo> assets = new LinkedHashSet<>();
 
     private String description;
 
-    public Listing(String ownerReference, String description) {
-        this.ownerReference = ownerReference;
-        this.description = description;
-    }
 }
