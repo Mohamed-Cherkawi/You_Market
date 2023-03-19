ec8eb5544097ac9b4118e7ca44694346bb415d40
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-19 18:22:35 +0000

    Create the VehicleListingRepository

--- src/main/java/org/coders/youmarket/repositories/VehicleListingRepository.java
+++ src/main/java/org/coders/youmarket/repositories/VehicleListingRepository.java
@@ -1,6 +1,6 @@
 package org.coders.youmarket.repositories;
 
-import org.coders.youmarket.entities.VehicleListing;
+import org.coders.youmarket.entities.vehicle.VehicleListing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
