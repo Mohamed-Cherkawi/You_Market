011c8fb23495768661925518c4337744088de8bc
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-20 15:54:46 +0000

    Modified & refactored code

--- src/main/java/org/coders/youmarket/services/implementations/AuthenticationService.java
+++ src/main/java/org/coders/youmarket/services/implementations/AuthenticationService.java
@@ -2,20 +2,25 @@ package org.coders.youmarket.services.implementations;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.coders.youmarket.entities.Address;
 import org.coders.youmarket.entities.AppUser;
 import org.coders.youmarket.enums.AvailabilityStateEnum;
 import org.coders.youmarket.services.dtos.auth.AuthenticationRequestDto;
 import org.coders.youmarket.services.dtos.auth.RegisterRequestDto;
 import org.coders.youmarket.services.dtos.auth.TokenRequestResponse;
+import org.coders.youmarket.services.dtos.response.ResponseHandler;
 import org.coders.youmarket.services.interfaces.AuthenticationServiceInterface;
 import org.coders.youmarket.services.interfaces.UserServiceInterface;
 import org.coders.youmarket.services.security.JwtService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 
@@ -28,11 +33,11 @@ public class AuthenticationService implements AuthenticationServiceInterface {
     private final AuthenticationManager authenticationManager;
 
     @Override
-    public TokenRequestResponse register(RegisterRequestDto request) {
+    public ResponseEntity<Object> register(RegisterRequestDto request) {
         boolean isUsernameExists = userService.isUsernameAlreadyExists(request.getUsername());
 
         if(isUsernameExists)
-            return null;
+            return ResponseHandler.generateResponse("This username is already taken try another one", HttpStatus.BAD_REQUEST);
 
         AppUser user = AppUser.builder()
                 .reference(UUID.randomUUID().toString())
@@ -41,22 +46,29 @@ public class AuthenticationService implements AuthenticationServiceInterface {
                 .name(request.getName())
                 .phone(request.getPhone())
                 .status(AvailabilityStateEnum.ONLINE)
+                .address(
+                        Address.builder()
+                                .title(request.getAddress().getTitle())
+                                .description(request.getAddress().getDescription())
+                                .build()
+                )
+                .createdAt(LocalDateTime.now())
                 .build();
 
         try {
             user = userService.saveUser(user);
             log.info("A New user was created successfully {}",user);
         }catch (Exception e){
-            return null;
+            return ResponseHandler.generateResponse("Something went wrong while saving the user , please try again", HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
         String jwtToken = jwtService.generateToken(user);
 
-        return getAuthResponse(jwtToken);
+        return ResponseHandler.generateResponse("The user was successfully created",HttpStatus.CREATED, getAuthResponse(jwtToken));
     }
 
     @Override
-    public Object authenticate(AuthenticationRequestDto request) {
+    public ResponseEntity<Object> authenticate(AuthenticationRequestDto request) {
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
@@ -65,17 +77,17 @@ public class AuthenticationService implements AuthenticationServiceInterface {
                     )
             );
         } catch(AuthenticationException e){
-            e.printStackTrace();
-            return "400";
+            log.info("The following username {} does not exists !",request.getUsername());
+            return ResponseHandler.generateResponse("Bad Credentials or the account is not even exists",HttpStatus.BAD_REQUEST);
         }
 
         AppUser user = userService.findUserByUsername(request.getUsername());
         if(user.getStatus().equals(AvailabilityStateEnum.BANNED))
-            return "403";
+            return ResponseHandler.generateResponse("You can't reach the application for now because the admin has banned you",HttpStatus.UNAUTHORIZED);
 
         String jwtToken = jwtService.generateToken(user);
 
-        return getAuthResponse(jwtToken);
+        return ResponseHandler.generateResponse("Authentication Succeed",HttpStatus.OK,getAuthResponse(jwtToken));
     }
     private TokenRequestResponse getAuthResponse(String token) {
         return TokenRequestResponse.builder()
