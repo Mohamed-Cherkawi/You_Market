232996961d3ec6217521e677f637c2cfb4037e88
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-20 16:01:06 +0000

    Change return type

--- src/main/java/org/coders/youmarket/services/interfaces/AuthenticationServiceInterface.java
+++ src/main/java/org/coders/youmarket/services/interfaces/AuthenticationServiceInterface.java
@@ -2,9 +2,9 @@ package org.coders.youmarket.services.interfaces;
 
 import org.coders.youmarket.services.dtos.auth.AuthenticationRequestDto;
 import org.coders.youmarket.services.dtos.auth.RegisterRequestDto;
-import org.coders.youmarket.services.dtos.auth.TokenRequestResponse;
+import org.springframework.http.ResponseEntity;
 
 public interface AuthenticationServiceInterface {
-    TokenRequestResponse register(RegisterRequestDto request);
-    Object authenticate(AuthenticationRequestDto request);
+    ResponseEntity<Object> register(RegisterRequestDto request);
+    ResponseEntity<Object> authenticate(AuthenticationRequestDto request);
 }
