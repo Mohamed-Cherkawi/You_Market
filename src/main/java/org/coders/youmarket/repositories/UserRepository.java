2d88ab0abe9fcca0ea6af2227dc29fcb301b71bd
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-20 15:49:53 +0000

    Add a find by reference method

--- src/main/java/org/coders/youmarket/repositories/UserRepository.java
+++ src/main/java/org/coders/youmarket/repositories/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByUsername(String username);
+    Optional<AppUser> findByReference(String reference);
 }
