8182df3cd4a879d9a8adaf2727cfa19d787d127d
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-20 16:01:27 +0000

    Create some new methods

--- src/main/java/org/coders/youmarket/services/interfaces/UserServiceInterface.java
+++ src/main/java/org/coders/youmarket/services/interfaces/UserServiceInterface.java
@@ -1,7 +1,7 @@
 package org.coders.youmarket.services.interfaces;
 
-
 import org.coders.youmarket.entities.AppUser;
+import org.coders.youmarket.services.dtos.user.ProfileUpdateRequest;
 
 import java.util.List;
 
@@ -9,5 +9,7 @@ public interface UserServiceInterface {
     List<AppUser> getAllUsers();
     AppUser saveUser(AppUser user);
     AppUser findUserByUsername(String username);
+    AppUser findUserByReference(String reference);
     boolean isUsernameAlreadyExists(String username);
+    AppUser updateUserProfileInfos(ProfileUpdateRequest profileUpdateRequest);
 }
