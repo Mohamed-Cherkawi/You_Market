f79b8874910535966a03b00bbdfd07c4ff77e7dc
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-20 15:53:44 +0000

    Create a custom response class that groups two methods , one that accepts the message , status and data , and the other one with only the message and status code to return them as responses to the client side

---
+++ src/main/java/org/coders/youmarket/services/dtos/response/ResponseHandler.java
@@ -0,0 +1,27 @@
+package org.coders.youmarket.services.dtos.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public class ResponseHandler {
+
+    private ResponseHandler() {
+
+    }
+
+    public static ResponseEntity<Object> generateResponse(String message, HttpStatus status, Object responseObj) {
+
+        return ResponseEntity.status(status).body(Map.of(
+                "message", message,
+                "statusCode", status.value(),
+                "statusName" , status.name(),
+                "data", responseObj)
+        );
+
+    }
+    public static ResponseEntity<Object> generateResponse(String message, HttpStatus status) {
+        return ResponseEntity.status(status).body(message);
+    }
+}
