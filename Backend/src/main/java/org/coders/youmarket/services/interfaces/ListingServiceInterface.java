0fe6d936a4f4b0967ba7b7bafd85b2895fddb7a0
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-21 15:57:21 +0000

    Create a new method and renamed one

--- Backend/src/main/java/org/coders/youmarket/services/interfaces/ListingServiceInterface.java
+++ Backend/src/main/java/org/coders/youmarket/services/interfaces/ListingServiceInterface.java
@@ -3,7 +3,8 @@ package org.coders.youmarket.services.interfaces;
 import org.springframework.http.ResponseEntity;
 
 public interface ListingServiceInterface {
-    ResponseEntity<Object> getAllListing();
+    ResponseEntity<Object> getAllListings();
+    ResponseEntity<Object> getAllListingsByOwnerReference(String ownerReference);
     ResponseEntity<Object> getListingByReference(String reference);
     ResponseEntity<Object> createListing();
     ResponseEntity<Object> updateListing();
