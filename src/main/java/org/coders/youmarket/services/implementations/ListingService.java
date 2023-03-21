604e1d8b10e71960d2da773cb436a2b6f5789504
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-21 14:38:25 +0000

    Write the business logic code required for the getListingByReference method

--- src/main/java/org/coders/youmarket/services/implementations/ListingService.java
+++ src/main/java/org/coders/youmarket/services/implementations/ListingService.java
@@ -7,12 +7,14 @@ import org.coders.youmarket.repositories.ListingRepository;
 import org.coders.youmarket.services.dtos.listing.ListingOverviewResponse;
 import org.coders.youmarket.services.interfaces.ListingServiceInterface;
 import org.coders.youmarket.util.EntityMapping;
+import org.coders.youmarket.util.HeaderKeyValueResponse;
 import org.coders.youmarket.util.ResponseHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -46,7 +48,22 @@ public class ListingService implements ListingServiceInterface {
 
     @Override
     public ResponseEntity<Object> getListingByReference(String reference) {
-        return null;
+        Optional<Listing> listing = listingRepository.findByListingReference(reference);
+
+        return listing.map(
+                value -> ResponseHandler.generateResponse(
+                        "The Listing Has Ben Fetched Successfully",
+                        HttpStatus.OK,
+                        value,
+                        HeaderKeyValueResponse.builder().
+                                key("Listing-Type").
+                                value(value.getListingType().name())
+                                .build())
+                )
+                .orElseGet(() -> ResponseHandler.generateResponse(
+                "There is no listing with the following reference : " + reference,
+                HttpStatus.BAD_REQUEST));
+
     }
 
     @Override
