3d177c3d6264b8e67934cbe3f7da0e9652e9a057
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-21 10:36:58 +0000

    Use linkedHashMap instead of a regular one to prevent shuffling the order of the keys

--- src/main/java/org/coders/youmarket/util/ResponseHandler.java
+++ src/main/java/org/coders/youmarket/util/ResponseHandler.java
@@ -3,9 +3,11 @@ package org.coders.youmarket.util;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class ResponseHandler {
+    private static Map<String,Object> map = new LinkedHashMap<>();
 
     private ResponseHandler() {
 
@@ -13,13 +15,12 @@ public class ResponseHandler {
 
     public static ResponseEntity<Object> generateResponse(String message, HttpStatus status, Object responseObj) {
 
-        return ResponseEntity.status(status).body(Map.of(
-                "message", message,
-                "statusCode", status.value(),
-                "statusName" , status.name(),
-                "data", responseObj)
-        );
+        map.put("message",message);
+        map.put("statusCode",status.value());
+        map.put("statusName",status.name());
+        map.put("data",responseObj);
 
+        return ResponseEntity.status(status).body(map);
     }
     public static ResponseEntity<Object> generateResponse(String message, HttpStatus status) {
         return ResponseEntity.status(status).body(message);
