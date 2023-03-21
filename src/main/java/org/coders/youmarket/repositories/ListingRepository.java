a1ce3f792b5ccf620795e74f0e73c66e48100765
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-21 14:37:14 +0000

    Create the method to find the listing by reference

--- src/main/java/org/coders/youmarket/repositories/ListingRepository.java
+++ src/main/java/org/coders/youmarket/repositories/ListingRepository.java
@@ -4,6 +4,9 @@ import org.coders.youmarket.entities.Listing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ListingRepository extends JpaRepository<Listing, Long> {
+    Optional<Listing> findByListingReference(String reference);
 }
