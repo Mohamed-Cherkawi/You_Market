af28af6b71c44e2b8028140cdce6b5ee5bf49124
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-21 10:18:20 +0000

    Add three important attributes

--- src/main/java/org/coders/youmarket/entities/Listing.java
+++ src/main/java/org/coders/youmarket/entities/Listing.java
@@ -3,6 +3,8 @@ package org.coders.youmarket.entities;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -16,6 +18,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import org.coders.youmarket.enums.listing.ListingTypeEnum;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
@@ -30,13 +33,25 @@ public class Listing {
     @Column(name = "id", nullable = false, unique = true)
     private Long id;
 
+    @Column(name = "listing_reference", nullable = false, length = 100)
+    private String listingReference;
+
+    @Column(name = "owner_reference", nullable = false, length = 100)
     private String ownerReference;
 
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "listing_id")
     private Set<Photo> assets = new LinkedHashSet<>();
 
+    @Column(name = "price", nullable = false)
+    private Float price;
+
+    @Column(name = "description")
     private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "listing_type", nullable = false, length = 7)
+    private ListingTypeEnum listingType;
 
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
