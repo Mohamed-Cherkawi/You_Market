c3714a1f2e77dafe5d7a2d38ec509be0cab01486
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-21 14:40:01 +0000

    Overloaded again the generateResponse method, so it can take also the headers this time , and do some refactoring

--- src/main/java/org/coders/youmarket/util/ResponseHandler.java
+++ src/main/java/org/coders/youmarket/util/ResponseHandler.java
@@ -1,5 +1,6 @@
 package org.coders.youmarket.util;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -8,21 +9,34 @@ import java.util.Map;
 
 public class ResponseHandler {
     private static Map<String,Object> map = new LinkedHashMap<>();
+    private static HttpHeaders responseHeaders = new HttpHeaders();
 
     private ResponseHandler() {
 
     }
 
     public static ResponseEntity<Object> generateResponse(String message, HttpStatus status, Object responseObj) {
+        putContentInMap(message,status,responseObj);
 
+        return ResponseEntity.status(status).body(map);
+    }
+
+    public static ResponseEntity<Object> generateResponse(String message, HttpStatus status,Object responseObj ,HeaderKeyValueResponse header) {
+        responseHeaders.set(header.getKey(),header.getValue());
+
+        putContentInMap(message, status, responseObj);
+
+        return ResponseEntity.status(status).headers(responseHeaders).body(map);
+    }
+
+    public static ResponseEntity<Object> generateResponse(String message, HttpStatus status) {
+        return ResponseEntity.status(status).body(message);
+    }
+
+    private static void putContentInMap(String message, HttpStatus status , Object responseObj){
         map.put("message",message);
         map.put("statusCode",status.value());
         map.put("statusName",status.name());
         map.put("data",responseObj);
-
-        return ResponseEntity.status(status).body(map);
-    }
-    public static ResponseEntity<Object> generateResponse(String message, HttpStatus status) {
-        return ResponseEntity.status(status).body(message);
     }
 }
