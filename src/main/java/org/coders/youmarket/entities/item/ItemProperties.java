db2b96ce9dfa7324ae162c3304cfd9f47125d640
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-21 10:17:42 +0000

    Package refactoring affect

--- src/main/java/org/coders/youmarket/entities/item/ItemProperties.java
+++ src/main/java/org/coders/youmarket/entities/item/ItemProperties.java
@@ -15,7 +15,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.coders.youmarket.enums.ListingConditionEnum;
+import org.coders.youmarket.enums.listing.ListingConditionEnum;
 import org.coders.youmarket.enums.item.ListingCategoryEnum;
 
 @Getter @Setter @ToString
