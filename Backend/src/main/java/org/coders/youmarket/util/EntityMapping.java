63558b9fe0fe69d56989e1355f77759e4dd71012
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-21 22:15:54 +0000

    Remove some spaces

--- Backend/src/main/java/org/coders/youmarket/util/EntityMapping.java
+++ Backend/src/main/java/org/coders/youmarket/util/EntityMapping.java
@@ -2,7 +2,9 @@ package org.coders.youmarket.util;
 
 import org.coders.youmarket.entities.AppUser;
 import org.coders.youmarket.entities.Listing;
+import org.coders.youmarket.entities.vehicle.VehicleListing;
 import org.coders.youmarket.services.dtos.listing.ListingOverviewResponse;
+import org.coders.youmarket.services.dtos.listing.vehicle.VehicleRequest;
 import org.coders.youmarket.services.dtos.user.ProfilePreviewResponse;
 import org.modelmapper.ModelMapper;
 
@@ -15,8 +17,14 @@ public class EntityMapping {
     public static ProfilePreviewResponse userToProfilePreviewResponse(AppUser user) {
         return modelMapper.map(user, ProfilePreviewResponse.class);
     }
-
     public static ListingOverviewResponse userToListingOverviewResponse(Listing listing) {
         return modelMapper.map(listing, ListingOverviewResponse.class);
+    }
+
+    public static VehicleRequest vehicleListingToVehicleRequest(VehicleListing vehicleListing){
+        return modelMapper.map(vehicleListing, VehicleRequest.class);
+    }
+    public static VehicleListing vehicleRequestToVehicleListing(VehicleRequest vehicleRequest){
+        return modelMapper.map(vehicleRequest, VehicleListing.class);
     }
 }
