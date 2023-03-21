47c0b0489c34580f0231627e7fe07f0a16679419
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-21 15:55:32 +0000

    Add the findAllListingsByOwnerReference method

--- Backend/src/main/java/org/coders/youmarket/repositories/ListingRepository.java
+++ Backend/src/main/java/org/coders/youmarket/repositories/ListingRepository.java
@@ -4,9 +4,11 @@ import org.coders.youmarket.entities.Listing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ListingRepository extends JpaRepository<Listing, Long> {
     Optional<Listing> findByListingReference(String reference);
+    List<Listing> findAllByOwnerReference(String ownerReference);
 }
