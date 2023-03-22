fd4556c6e30fd0dfa7265cbaf78ce6b6d10e3603
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-22 17:32:06 +0000

    Modified the base parent transfer object that contains all the attributes related in each listing type , to prevent redundant code

--- Backend/src/main/java/org/coders/youmarket/services/dtos/listing/ListingRequestResponse.java
+++ Backend/src/main/java/org/coders/youmarket/services/dtos/listing/ListingRequestResponse.java
@@ -3,13 +3,18 @@ package org.coders.youmarket.services.dtos.listing;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.coders.youmarket.enums.listing.ListingTypeEnum;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Getter @Setter @ToString
-public class ListingOverviewResponse {
+public class ListingRequestResponse {
     private String listingReference;
+    private String ownerReference;
     private Set<String> assets;
-    private Float price;
+    private String price;
     private String description;
+    private ListingTypeEnum listingType;
+    private LocalDateTime updatedAt;
 }
