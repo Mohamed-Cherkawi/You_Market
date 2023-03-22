b95c3f189ae77e133642213e06067f4d79450f00
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-22 18:20:23 +0000

    Add the findByListingReferenceMethod

--- Backend/src/main/java/org/coders/youmarket/repositories/VehicleListingRepository.java
+++ Backend/src/main/java/org/coders/youmarket/repositories/VehicleListingRepository.java
@@ -4,5 +4,9 @@ import org.coders.youmarket.entities.vehicle.VehicleListing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface VehicleListingRepository extends JpaRepository<VehicleListing,Long> { }
+public interface VehicleListingRepository extends JpaRepository<VehicleListing,Long> {
+    Optional<VehicleListing> findByListingReference(String reference);
+}
