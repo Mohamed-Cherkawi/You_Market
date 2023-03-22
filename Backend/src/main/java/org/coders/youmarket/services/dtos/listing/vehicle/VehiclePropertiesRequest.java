48446aa2634a6c57127ccc7e53be99e113ea0f4e
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-22 17:32:37 +0000

    Create the VehiclePropertiesRequest dto

---
+++ Backend/src/main/java/org/coders/youmarket/services/dtos/listing/vehicle/VehiclePropertiesRequest.java
@@ -0,0 +1,21 @@
+package org.coders.youmarket.services.dtos.listing.vehicle;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.coders.youmarket.enums.vehicle.VehicleBodyStyleEnum;
+import org.coders.youmarket.enums.vehicle.VehicleConditionEnum;
+import org.coders.youmarket.enums.vehicle.VehicleFuelType;
+import org.coders.youmarket.enums.vehicle.VehicleTypeEnum;
+
+@Getter @Setter
+public class VehiclePropertiesRequest {
+    private String make;
+    private String model;
+    private String mileage;
+    private VehicleTypeEnum type;
+    private VehicleBodyStyleEnum bodyStyle;
+    private String hasCleanTitle;
+    private VehicleConditionEnum condition;
+    private VehicleFuelType fuelType;
+    private String isManual;
+}
