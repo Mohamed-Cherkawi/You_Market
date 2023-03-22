44978726310ac04a68f293ee664528b99864aa59
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-22 18:22:13 +0000

    Add another method to parse a given LocalDateTime date to a formatted readable string date to be returned back

--- Backend/src/main/java/org/coders/youmarket/util/DateTimeParser.java
+++ Backend/src/main/java/org/coders/youmarket/util/DateTimeParser.java
@@ -1,11 +1,17 @@
 package org.coders.youmarket.util;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateTimeParser {
     private DateTimeParser(){}
 
     public static LocalDate getDateFromFormatPattern(String date){
         return LocalDate.parse(date);
+    }
+    public static String getStringDateOfLocalDateTimeStandardPattern(LocalDateTime date){
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        return date.format(myFormatObj);
     }
 }
