fbe18a55f205dbc0fa13431615460eb9dd73648c
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-23 17:16:01 +0000

    Add the findByListingReference method

--- Backend/src/main/java/org/coders/youmarket/repositories/ItemListingRepository.java
+++ Backend/src/main/java/org/coders/youmarket/repositories/ItemListingRepository.java
@@ -4,6 +4,9 @@ import org.coders.youmarket.entities.item.ItemListing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ItemListingRepository extends JpaRepository<ItemListing ,Long> {
+    Optional<ItemListing> findByListingReference(String reference);
 }
