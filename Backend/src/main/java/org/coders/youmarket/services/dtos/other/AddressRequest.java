160de56ab3ca7e7920ec9a9b5573f75bac4eb7eb
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-23 11:56:44 +0000

    Remove To String

--- Backend/src/main/java/org/coders/youmarket/services/dtos/other/AddressRequest.java
+++ Backend/src/main/java/org/coders/youmarket/services/dtos/other/AddressRequest.java
@@ -2,9 +2,8 @@ package org.coders.youmarket.services.dtos.other;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
-@Getter @Setter @ToString
+@Getter @Setter
 public class AddressRequest {
     private String id;
     private String title;
