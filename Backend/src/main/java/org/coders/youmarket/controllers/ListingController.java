d8a8cd585cacb8e9aeb5cf681e84cc13f7c597f4
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-23 10:58:37 +0000

    Create the deleteListingByReferenceApi method which gets that reference from the request header with the key Listing-Reference

--- Backend/src/main/java/org/coders/youmarket/controllers/ListingController.java
+++ Backend/src/main/java/org/coders/youmarket/controllers/ListingController.java
@@ -3,8 +3,10 @@ package org.coders.youmarket.controllers;
 import lombok.RequiredArgsConstructor;
 import org.coders.youmarket.services.interfaces.ListingServiceInterface;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,10 @@ public class ListingController {
     @GetMapping("/fetching-by-owner/single/{ownerReference}")
     public ResponseEntity<Object> getAllListingsByOwnerReferenceApi(@PathVariable("ownerReference") String ownerReference){
         return listingService.getAllListingsByOwnerReference(ownerReference);
+    }
+
+    @DeleteMapping("/deleting")
+    public ResponseEntity<Object> deleteListingByReferenceApi(@RequestHeader("Listing-Reference") String reference){
+        return listingService.deleteListing(reference);
     }
 }
