5bdf15f49c35d977f05bb6a75575d1157f8c9553
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-23 11:58:59 +0000

    Add two mappers methods , ItemListing towards ItemRequest or vice versa

--- Backend/src/main/java/org/coders/youmarket/util/EntityMapping.java
+++ Backend/src/main/java/org/coders/youmarket/util/EntityMapping.java
@@ -3,9 +3,11 @@ package org.coders.youmarket.util;
 import org.coders.youmarket.entities.AppUser;
 import org.coders.youmarket.entities.Listing;
 import org.coders.youmarket.entities.Photo;
+import org.coders.youmarket.entities.item.ItemListing;
 import org.coders.youmarket.entities.vehicle.VehicleListing;
 import org.coders.youmarket.entities.vehicle.VehicleProperties;
 import org.coders.youmarket.services.dtos.listing.ListingRequestResponse;
+import org.coders.youmarket.services.dtos.listing.item.ItemRequest;
 import org.coders.youmarket.services.dtos.listing.vehicle.VehiclePropertiesRequest;
 import org.coders.youmarket.services.dtos.listing.vehicle.VehicleRequest;
 import org.coders.youmarket.services.dtos.other.PhotoRequest;
@@ -31,9 +33,15 @@ public class EntityMapping {
     public static VehicleListing vehicleRequestToVehicleListing(VehicleRequest vehicleRequest){
         return modelMapper.map(vehicleRequest, VehicleListing.class);
     }
-
     public static VehicleProperties vehiclePropertiesRequestToVehicleProperties(VehiclePropertiesRequest vehiclePropertiesRequest){
         return modelMapper.map(vehiclePropertiesRequest, VehicleProperties.class);
+    }
+
+    public static ItemRequest itemListingToItemRequest(ItemListing itemListing){
+        return modelMapper.map(itemListing, ItemRequest.class);
+    }
+    public static ItemListing itemRequestToItemListing(ItemRequest itemRequest){
+        return modelMapper.map(itemRequest, ItemListing.class);
     }
 
     public static Photo photoRequestToPhoto(PhotoRequest photoRequest){
