c26cf5a596e76d76c1a9c2f000cf1fd1d0e6645f
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-24 11:55:30 +0000

    Create the HomeRentalRepository

--- Backend/src/main/java/org/coders/youmarket/repositories/HomeRentalRepository.java
+++ Backend/src/main/java/org/coders/youmarket/repositories/HomeRentalRepository.java
@@ -4,5 +4,9 @@ import org.coders.youmarket.entities.home.HomeRentalListing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface HomeRentalRepository extends JpaRepository<HomeRentalListing,Long> {}
+public interface HomeRentalRepository extends JpaRepository<HomeRentalListing,Long> {
+    Optional<HomeRentalListing> findByListingReference(String reference);
+}
