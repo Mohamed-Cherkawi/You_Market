9c7193096c445b992781a4d1ecfd6a2e9ec4eaa7
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-25 12:40:48 +0000

    Add the findAllByListingType method

--- Backend/src/main/java/org/coders/youmarket/repositories/ListingRepository.java
+++ Backend/src/main/java/org/coders/youmarket/repositories/ListingRepository.java
@@ -1,6 +1,7 @@
 package org.coders.youmarket.repositories;
 
 import org.coders.youmarket.entities.Listing;
+import org.coders.youmarket.enums.listing.ListingTypeEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,5 @@ public interface ListingRepository extends JpaRepository<Listing, Long> {
     Optional<Listing> findByListingReference(String reference);
     List<Listing> findAllByOwnerReference(String ownerReference);
     Integer deleteByListingReference(String reference);
+    List<Listing> findAllByListingType(ListingTypeEnum listingType);
 }
