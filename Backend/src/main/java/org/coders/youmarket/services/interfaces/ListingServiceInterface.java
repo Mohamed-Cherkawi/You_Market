8fe1443c02fc358fc4bb9f27f9a4dd0d3dada684
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-25 12:41:48 +0000

    Add the getAllListingsByListingType

--- Backend/src/main/java/org/coders/youmarket/services/interfaces/ListingServiceInterface.java
+++ Backend/src/main/java/org/coders/youmarket/services/interfaces/ListingServiceInterface.java
@@ -1,9 +1,11 @@
 package org.coders.youmarket.services.interfaces;
 
+import org.coders.youmarket.enums.listing.ListingTypeEnum;
 import org.springframework.http.ResponseEntity;
 
 public interface ListingServiceInterface {
     ResponseEntity<Object> getAllListings();
+    ResponseEntity<Object> getAllListingsByListingType(ListingTypeEnum listingType);
     ResponseEntity<Object> getAllListingsByOwnerReference(String ownerReference);
     ResponseEntity<Object> getListingByReference(String reference);
     ResponseEntity<Object> deleteListing(String reference);
