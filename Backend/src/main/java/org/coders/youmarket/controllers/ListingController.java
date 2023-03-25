45df3c9e42025c734bf413836eb8c3b8b89764d8
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-25 12:39:19 +0000

    Create the getAllListingsByListingTypeApi method

--- Backend/src/main/java/org/coders/youmarket/controllers/ListingController.java
+++ Backend/src/main/java/org/coders/youmarket/controllers/ListingController.java
@@ -1,6 +1,7 @@
 package org.coders.youmarket.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.coders.youmarket.enums.listing.ListingTypeEnum;
 import org.coders.youmarket.services.interfaces.ListingServiceInterface;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,14 +20,18 @@ public class ListingController {
     public ResponseEntity<Object> getAllListingsApi(){
         return listingService.getAllListings();
     }
+    @GetMapping("/fetching/all/by-categorie/{listingType}")
+    public ResponseEntity<Object> getAllListingsByListingTypeApi(@PathVariable ListingTypeEnum listingType){
+        return listingService.getAllListingsByListingType(listingType);
+    }
 
     @GetMapping("/fetching/single/{listingReference}")
-    public ResponseEntity<Object> getListingByReferenceApi(@PathVariable("listingReference") String listingReference){
+    public ResponseEntity<Object> getListingByReferenceApi(@PathVariable String listingReference){
         return listingService.getListingByReference(listingReference);
     }
 
     @GetMapping("/fetching-by-owner/single/{ownerReference}")
-    public ResponseEntity<Object> getAllListingsByOwnerReferenceApi(@PathVariable("ownerReference") String ownerReference){
+    public ResponseEntity<Object> getAllListingsByOwnerReferenceApi(@PathVariable String ownerReference){
         return listingService.getAllListingsByOwnerReference(ownerReference);
     }
 
