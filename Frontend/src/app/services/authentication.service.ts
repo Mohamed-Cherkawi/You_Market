fa8c3431c4c6cd79b5685c0b1fdd487768b0c758
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-26 11:45:30 +0000

    Create the authentication service

---
+++ Frontend/src/app/services/authentication.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {AuthenticationRequest} from "../models/auth/authentication-request.model";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {ResponseEntity} from "../utils/response-entity.model";
+import {AuthenticationResponse} from "../models/auth/authentication-response.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  private restPath: string = environment.apisBaseUrl + "/api/authenticate";
+  constructor(private http: HttpClient) {}
+
+  // public  register(request: RegisterRequest): Observable< AuthenticationResponse> {
+  //   return this.http.post<AuthenticationResponse>(`${this.restPath}/register`,request);
+  // }
+  public  authenticate(request: AuthenticationRequest): Observable<ResponseEntity<AuthenticationResponse>> {
+    return this.http.post<ResponseEntity<AuthenticationResponse>>(`${this.restPath}/login`, request);
+  }
+}
