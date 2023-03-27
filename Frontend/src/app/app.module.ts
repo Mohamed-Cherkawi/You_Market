9fb234d0397a3e4f45ff07f1d47b704340472183
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-27 17:52:57 +0000

    Add required modules

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,35 +4,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {LoginComponent} from "./modules/login/login.component";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {HttpClientModule} from "@angular/common/http";
 import {RouterOutlet} from "@angular/router";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {JWT_OPTIONS, JwtModule} from "@auth0/angular-jwt";
+import { RegisterComponent } from './modules/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    MatInputModule,
-    HttpClientModule,
-    RouterOutlet,
-    AppRoutingModule,
-    JwtModule.forRoot({
-      jwtOptionsProvider: { // this provider is necessary to use the librairy that decoes and manages the JWT
-        provide: JWT_OPTIONS,
-        useValue: {
-        }
-      }
-    })
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+        HttpClientModule,
+        RouterOutlet,
+        AppRoutingModule,
+        JwtModule.forRoot({
+            jwtOptionsProvider: { // this provider is necessary to use the librairy that decoes and manages the JWT
+                provide: JWT_OPTIONS,
+                useValue: {}
+            }
+        }),
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
