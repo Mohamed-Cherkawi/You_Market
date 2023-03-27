8a019b866f1315adc8d8ba4a0f4816c0563b7998
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-27 09:39:46 +0000

    Add some required modules

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {LoginComponent} from "./modules/login/login.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterOutlet} from "@angular/router";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {JWT_OPTIONS, JwtModule} from "@auth0/angular-jwt";
 
 @NgModule({
   declarations: [
@@ -12,7 +18,20 @@ import {LoginComponent} from "./modules/login/login.component";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    HttpClientModule,
+    RouterOutlet,
+    AppRoutingModule,
+    JwtModule.forRoot({
+      jwtOptionsProvider: { // this provider is necessary to use the librairy that decoes and manages the JWT
+        provide: JWT_OPTIONS,
+        useValue: {
+        }
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
