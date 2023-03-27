fab252a131491876e66c180c2eb6a7e16f44bcb9
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-27 17:53:11 +0000

    Add path for register route

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./modules/login/login.component";
+import {RegisterComponent} from "./modules/register/register.component";
 
 const routes: Routes = [
-  { path : 'login' , component: LoginComponent },
+  { path : 'login' , component : LoginComponent },
+  { path : 'register' , component : RegisterComponent },
 ];
 
 @NgModule({
