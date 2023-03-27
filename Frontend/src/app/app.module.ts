eb80e3e1a19e3ecd4e1aaf63a0bb575457b3b1ae
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-27 23:53:17 +0000

    commit some raw changes

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {LoginComponent} from "./modules/login/login.component";
+import { LoginComponent } from './modules/login/login.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {MatInputModule} from "@angular/material/input";
-import {HttpClientModule} from "@angular/common/http";
 import {RouterOutlet} from "@angular/router";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
 import {JWT_OPTIONS, JwtModule} from "@auth0/angular-jwt";
-import { RegisterComponent } from './modules/register/register.component';
+import {MatButtonModule} from "@angular/material/button";
+import {RegisterComponent} from "./modules/register/register.component";
 
 @NgModule({
   declarations: [
@@ -18,23 +21,25 @@ import { RegisterComponent } from './modules/register/register.component';
     LoginComponent,
     RegisterComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        ReactiveFormsModule,
-        BrowserAnimationsModule,
-        MatInputModule,
-        HttpClientModule,
-        RouterOutlet,
-        AppRoutingModule,
-        JwtModule.forRoot({
-            jwtOptionsProvider: { // this provider is necessary to use the librairy that decoes and manages the JWT
-                provide: JWT_OPTIONS,
-                useValue: {}
-            }
-        }),
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterOutlet,
+    ReactiveFormsModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    JwtModule.forRoot({
+      jwtOptionsProvider: { // this provider is necessary to use the librairy that decoes and manages the JWT
+        provide: JWT_OPTIONS,
+        useValue: {}
+      }
+    }),
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
