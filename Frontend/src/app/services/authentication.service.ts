35ec6b9851b413114ea51b2c25de614c00d9a1a4
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-28 12:41:31 +0000

    Add the register method that call the register api

--- Frontend/src/app/services/authentication.service.ts
+++ Frontend/src/app/services/authentication.service.ts
@@ -5,18 +5,19 @@ import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {ResponseEntity} from "../utils/response-entity.model";
 import {AuthenticationResponse} from "../models/auth/authentication-response.model";
+import {RegisterRequest} from "../models/auth/register-request.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  private restPath: string = environment.apisBaseUrl + "/api/authenticate";
+  private restPath: string = environment.apisBaseUrl + "/api/authenticate/";
   constructor(private http: HttpClient) {}
 
-  // public  register(request: RegisterRequest): Observable< AuthenticationResponse> {
-  //   return this.http.post<AuthenticationResponse>(`${this.restPath}/register`,request);
-  // }
+  public  register(request: RegisterRequest): Observable<ResponseEntity<AuthenticationResponse>> {
+    return this.http.post<ResponseEntity<AuthenticationResponse>>(`${this.restPath}register`,request);
+  }
   public  authenticate(request: AuthenticationRequest): Observable<ResponseEntity<AuthenticationResponse>> {
-    return this.http.post<ResponseEntity<AuthenticationResponse>>(`${this.restPath}/login`, request);
+    return this.http.post<ResponseEntity<AuthenticationResponse>>(`${this.restPath}login`, request);
   }
 }
