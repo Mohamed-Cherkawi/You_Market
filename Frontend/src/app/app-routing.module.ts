80af738aa00be20b3e07c109fa40ef210131107d
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-28 12:42:42 +0000

    create a default route which is the login page

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./modules/login/login.component";
 import {RegisterComponent} from "./modules/register/register.component";
+import {AuthenticationGuard} from "./guards/authentication.guard";
 
 const routes: Routes = [
-  { path : 'login' , component : LoginComponent },
-  { path : 'register' , component : RegisterComponent },
+  { path : '', redirectTo: '/login', pathMatch: 'full' },
+  { path : 'login' , component : LoginComponent , canActivate : [AuthenticationGuard] },
+  { path : 'register' , component : RegisterComponent , canActivate : [AuthenticationGuard] },
+  { path : 'home' , component : RegisterComponent  }
 ];
 
 @NgModule({
