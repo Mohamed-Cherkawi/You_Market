64c9f928d00fbbe3b5c0e3aba88a449063355cf5
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-28 12:41:06 +0000

    Create the register method functionality

--- Frontend/src/app/modules/register/register.component.ts
+++ Frontend/src/app/modules/register/register.component.ts
@@ -2,6 +2,12 @@ import * as countriesDataJson from '../../utils/json/countries-apis.json';
 
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {RegisterRequest} from "../../models/auth/register-request.model";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/authentication.service";
+import {ResponseEntity} from "../../utils/response-entity.model";
+import {AuthenticationResponse} from "../../models/auth/authentication-response.model";
+import {HttpErrorResponse} from "@angular/common/http";
 
 declare const focus: Function;
 declare const blur: Function;
@@ -17,9 +23,12 @@ export class RegisterComponent implements OnInit {
   credentialsError: boolean = false;
   registerForm!: FormGroup;
   countries!: string[];
+  registerRequest!: RegisterRequest;
 
   constructor(
-    private formBuilder: FormBuilder,) { }
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthenticationService) { }
 
   ngOnInit(): void {
     this.getCountriesDataFromJsonObject();
@@ -31,17 +40,48 @@ export class RegisterComponent implements OnInit {
       password: ['', [Validators.required]],
       country: ['',[Validators.required]]
     });
-    console.log(this.countries)
   }
 
   public onRegister(): void{
-    console.log(this.registerForm.value.phone);
-    console.log(this.registerForm.value.name);
-    console.log(this.registerForm.value.country);
-    console.log(this.registerForm.value.username);
-    console.log(this.registerForm.value.password);
 
-    this.validateFormInputs();
+    if(! this.registerForm.valid){
+        this.credentialsError = true;
+        return;
+    }
+
+    this.handleFormButton();
+
+    this.registerRequest = {
+      name : this.registerForm.value.name,
+      phone : this.registerForm.value.phone,
+      address : {
+        title : this.registerForm.value.country,
+      },
+      username : this.registerForm.value.username,
+      password : this.registerForm.value.password,
+    }
+
+    this.authService.register(this.registerRequest)
+      .subscribe(
+      {
+        next:
+          (response: ResponseEntity<AuthenticationResponse>) => {
+            console.log(response)
+
+            const token = response.data.token;
+            localStorage.setItem('token',token);
+
+            this.redirectToHome();
+          },
+        error:
+        (error : HttpErrorResponse) => {
+          this.credentialsError = true;
+            console.log(error.error);
+        }
+      });
+  }
+  private redirectToHome(): void{
+    this.router.navigate(['/home']);
   }
   public onInputFocus(parentElementId: string): void {
     focus(parentElementId);
@@ -49,41 +89,22 @@ export class RegisterComponent implements OnInit {
   public onInputBlur(parentElementId: string , formNumber: number): void {
     switch (formNumber){
       case 1 :
-        if(parentElementId == "fullNameField"){
-
-          if(this.registerForm.value.name){
-            return;
-          }
-        }else {
-          if(this.registerForm.value.phone){
-            return;
-          }
-        }
-        blur(parentElementId);
-
-        if(!this.registerForm.value.name && !this.registerForm.value.phone){
-          this.credentialsError = false;
-        }
+        this.onBlurredFirstForm(parentElementId);
         break;
       case 2 :
-        if(parentElementId == "usernameField"){
-
-          if(this.registerForm.value.username){
-            return;
-          }
-        }else {
-          if(this.registerForm.value.password){
-            return;
-          }
-        }
-        blur(parentElementId);
-
-        if(!this.registerForm.value.username && !this.registerForm.value.password){
-          this.credentialsError = false;
-        }
+        this.onBlurredSecondForm(parentElementId);
     }
   }
+  private handleFormButton(): void {
+    const button = document.querySelector("button");
+    button!.disabled = true;
+    button!.style.opacity = "0.5";
 
+    setTimeout(function() {
+      button!.disabled = false;
+      button!.style.opacity = "1";
+    }, 1000);
+  }
   public triggerNextOrPreviousButton(): void{
     const form1 = document.getElementById('form-1');
     const form2 = document.getElementById('form-2');
@@ -98,19 +119,45 @@ export class RegisterComponent implements OnInit {
         this.nextPrevButt = 'Next';
       }
   }
-  private validateFormInputs(): void {
-    if(! this.registerForm.value.name
-      || ! this.registerForm.value.phone
-      || ! this.registerForm.value.country
-      || ! this.registerForm.value.username
-      || ! this.registerForm.value.password){
-      this.credentialsError = true;
+  private onBlurredFirstForm(parentElementId: string): void {
+    if(parentElementId == "fullNameField"){
+
+      if(this.registerForm.value.name){
+        return;
+      }
+    }else {
+      if(this.registerForm.value.phone){
+        return;
+      }
+    }
+    blur(parentElementId);
+
+    if(!this.registerForm.value.name && !this.registerForm.value.phone){
+      this.credentialsError = false;
+    }
+  }
+  private onBlurredSecondForm(parentElementId: string): void {
+    if(parentElementId == "usernameField"){
+
+      if(this.registerForm.value.username){
+        return;
+      }
+    }else {
+      if(this.registerForm.value.password){
+        return;
+      }
+    }
+    blur(parentElementId);
+
+    if(!this.registerForm.value.username && !this.registerForm.value.password){
+      this.credentialsError = false;
     }
   }
   private getCountriesDataFromJsonObject():void {
     this.countries = Array.from(
-        Object.values(countriesDataJson).map((value) => value as string)
+        Object.values(countriesDataJson).map((value) => value)
     );
-    this.countries[252] = '-- Select a country --';
+    this.countries.sort();
+    this.countries.splice(251,1);
   }
 }
