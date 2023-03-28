080b3caf7cdbb60b5492bce1b067ebf9cf5bb871
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-28 22:56:14 +0000

    Give the HomeComponent to the home route

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./modules/login/login.component";
 import {RegisterComponent} from "./modules/register/register.component";
 import {AuthenticationGuard} from "./guards/authentication.guard";
+import {HomeComponent} from "./modules/home/home.component";
 
 const routes: Routes = [
   { path : '', redirectTo: '/login', pathMatch: 'full' },
   { path : 'login' , component : LoginComponent , canActivate : [AuthenticationGuard] },
   { path : 'register' , component : RegisterComponent , canActivate : [AuthenticationGuard] },
-  { path : 'home' , component : RegisterComponent  }
+  { path : 'home' , component : HomeComponent  }
 ];
 
 @NgModule({
