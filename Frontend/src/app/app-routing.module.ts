4381186a9229dc4776a9db4d644f55d15c0fd5d7
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-04-01 11:00:48 +0000

    Add the create route

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {LoginComponent} from "./modules/login/login.component";
 import {RegisterComponent} from "./modules/register/register.component";
 import {AuthenticationGuard} from "./guards/authentication.guard";
 import {HomeComponent} from "./modules/home/home.component";
+import {CreateListingComponent} from "./modules/create-listing/create-listing.component";
 
 const routes: Routes = [
   { path : '', redirectTo: '/login', pathMatch: 'full' },
   { path : 'login' , component : LoginComponent , canActivate : [AuthenticationGuard] },
   { path : 'register' , component : RegisterComponent , canActivate : [AuthenticationGuard] },
-  { path : 'home' , component : HomeComponent  }
+  { path : 'home' , component : HomeComponent  },
+  { path : 'create' , component : CreateListingComponent  },
 ];
 
 @NgModule({
