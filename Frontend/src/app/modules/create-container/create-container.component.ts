ce4f74d18fee6c71ed128e849957b239247e3cd4
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-04-02 12:27:16 +0000

    Create a container component for the create parent route to use its child routes ( router-outlet )

---
+++ Frontend/src/app/modules/create-container/create-container.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-container',
+  templateUrl: './create-container.component.html',
+  styleUrls: ['./create-container.component.css']
+})
+export class CreateContainerComponent {}
