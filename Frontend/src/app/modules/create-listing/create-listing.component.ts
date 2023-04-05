a75b670d08f30a530f6a950777d5b134e3b75923
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-04-05 23:17:19 +0000

    Create the redirect to specific route method

--- Frontend/src/app/modules/create-listing/create-listing.component.ts
+++ Frontend/src/app/modules/create-listing/create-listing.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-create-listing',
@@ -8,14 +8,17 @@ import {Router} from "@angular/router";
 })
 export class CreateListingComponent {
   listingsTypeProperties = [
-    { title : 'Item for sale' , description : 'Create a single listing<br>for one or more items to sell'  , imageName: 'open-box' , alt : 'Items Image'},
-    { title : 'Vehicle for sale' , description : 'Sell a car , truck or <br> other type of vehicle' , imageName: 'car' , alt : 'Car Image'},
-    { title : 'Home for Rent' , description : 'List a house or apartment<br>for rent' , imageName: 'home' , alt : 'House Image'},
+    { title : 'Item for sale' , description : 'Create a single listing<br>for one or more items to sell'  , imageName: 'open-box' , alt : 'Items Image' , redirectTo: 'item'},
+    { title : 'Vehicle for sale' , description : 'Sell a car , truck or <br> other type of vehicle' , imageName: 'car' , alt : 'Car Image' , redirectTo: 'vehicle'},
+    { title : 'Home for Rent' , description : 'List a house or apartment<br>for rent' , imageName: 'home' , alt : 'House Image' , redirectTo: 'home'},
   ];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router , private activatedRoute: ActivatedRoute) { }
 
   redirectToHome(): void {
     this.router.navigate(['/home']);
+  }
+  redirectToRoute(route: string): void {
+    this.router.navigate([route] , { relativeTo: this.activatedRoute });
   }
 }
