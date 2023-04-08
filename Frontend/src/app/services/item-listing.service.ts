9682f5a92612524744c55ef64f8c310c19546ffa
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-04-08 17:05:27 +0000

    Built the service responsible for item listing

---
+++ Frontend/src/app/services/item-listing.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {ResponseEntity} from "../utils/response-entity.model";
+import {ItemListing} from "../models/listings/item/item-listing.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemListingService {
+  private restPath: string = environment.apisBaseUrl + "/api/listing/item/";
+  constructor(private http: HttpClient) {}
+
+  public  createItemListing(request: FormData): Observable<ResponseEntity<ItemListing>> {
+    return this.http.post<ResponseEntity<ItemListing>>(`${this.restPath}creating`,request);
+  }
+}
