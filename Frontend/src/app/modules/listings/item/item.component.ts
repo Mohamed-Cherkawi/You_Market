83bc838114b071ecdf49d26c1acc6c4299672500
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-04-08 17:05:02 +0000

    Add some new FormGroup items to the form & add many methods to handle that drag and drop functionality , along with setting up the register to accept inputs value and send it to the service

--- Frontend/src/app/modules/listings/item/item.component.ts
+++ Frontend/src/app/modules/listings/item/item.component.ts
@@ -1,7 +1,14 @@
 import * as categoriesJsonData from '../../../utils/json/item-listing-categories.json';
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ItemListingCategory} from "../../../models/listings/item-listing-category.model";
+import {DomSanitizer} from "@angular/platform-browser";
+import {FileHandle} from "../../../models/other/file-handle.model";
+import {ItemListing} from "../../../models/listings/item/item-listing.model";
+import {ListingType} from "../../../enums/listing-type.enum";
+import {ItemListingService} from "../../../services/item-listing.service";
+import {ResponseEntity} from "../../../utils/response-entity.model";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-item',
@@ -9,21 +16,126 @@ import {ItemListingCategory} from "../../../models/listings/item-listing-categor
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit{
-
+  @ViewChild('ItemListingForm') ItemListingForm!: ElementRef;
+  meetupPreferences = [
+    { iconName : 'public' , title : 'Public meetup' , resume : 'Meet at a public location.' , alt : 'P Icon' , formControlName: 'isPublicMeetup'},
+    { iconName : 'door' , title : 'Door pickup' , resume : 'Buyer picks up at your door.' , alt : 'Door Icon' , formControlName: 'isDoorPickup' },
+    { iconName : 'door' , title : 'Door dropoff' , resume : 'you drop off at buyers \'s door.' , alt : 'Door Icon' , formControlName: 'isDoorDropoff'},
+  ];
+  listingConditions = [
+    { preview: 'New' , value : 'NEW' },
+    { preview: 'Used - Like New' , value : 'USED_LIKE_NEW' },
+    { preview: 'Used - Good' , value : 'USED_GOOD' },
+    { preview: 'Used - Fair' , value : 'USED_FAIR' },
+  ];
   itemForm! : FormGroup;
   categories!: ItemListingCategory[];
-  constructor(private formBuilder: FormBuilder) {
+  filesArrayHolder: FileHandle[] = [];
+
+  constructor(private formBuilder: FormBuilder ,
+              private itemListingService: ItemListingService,
+              private sanitizer: DomSanitizer) {
   }
 
   ngOnInit(): void {
     this.itemForm = this.formBuilder.group({
         title: ['', [Validators.required , Validators.maxLength(99)]],
         price: ['', [Validators.required]] ,
+        imageFileName: [[] , [Validators.required]],
         category: ['' , [Validators.required]],
+        condition: ['' , [Validators.required]],
+        brand: ['' , [Validators.maxLength(99)]],
         description: ['' , [Validators.required , Validators.maxLength(255)]],
+        sku: ['' , [Validators.maxLength(99)]],
+        isPublicMeetup: [false],
+        isDoorPickup: [false],
+        isDoorDropoff: [false]
       });
 
     this.getCategoriesDataFromJsonObject();
+  }
+
+  onSubmit() {
+    //this.ItemListingForm.nativeElement.submit();
+
+    if(!this.itemForm.valid){
+      console.log('ooops');
+      return;
+    }else {
+      console.log('fine')
+    }
+
+    this.itemListingService.createItemListing(
+      this.prepareFormData(
+        this.prepareItemListingObject()
+      ))
+      .subscribe(
+        {
+          next:
+            (response: ResponseEntity<ItemListing>) => {
+              console.log(response);
+            },
+          error:
+            (error : HttpErrorResponse) => {
+              console.log(error.error)
+            }
+        });
+  }
+
+  private prepareItemListingObject(): ItemListing {
+    return  {
+      fileHandling: this.filesArrayHolder,
+      ownerReference: 'HDHJD38638363JDH',
+      price: this.itemForm.get('price')?.value,
+      description: this.itemForm.get('description')?.value,
+      listingType: ListingType.ITEM,
+      title: this.itemForm.get('title')?.value,
+      location: {
+        title: 'Marrakech',
+        description: 'For test purposes'
+      },
+      publicMeetup: this.itemForm.get('isPublicMeetup')?.value,
+      doorDropOff: this.itemForm.get('isDoorDropoff')?.value,
+      doorPickup: this.itemForm.get('isDoorPickup')?.value,
+      properties: {
+        category: this.itemForm.get('category')?.value,
+        condition: this.itemForm.get('condition')?.value,
+        brand: this.itemForm.get('brand')?.value,
+        sku: this.itemForm.get('sku')?.value
+      }
+    };
+  }
+  private prepareFormData(listing: ItemListing): FormData {
+    const formData = new FormData();
+
+    formData.append(
+      'item',
+      new Blob([JSON.stringify(listing)] , { type: 'application/json' })
+    );
+
+    listing.fileHandling.forEach((file: FileHandle) => {
+        formData.append(
+          'imagesFiles',
+          file.file,
+          file.file.name
+        );
+    });
+
+    return formData;
+  }
+
+  onFileSelected(event: any) {
+
+    for (let file of event.target.files) {
+          this.filesArrayHolder.push({
+            file: file,
+            url: this.sanitizer.bypassSecurityTrustUrl(
+              window.URL.createObjectURL(file)
+            )
+          });
+      }
+
+    console.log(this.filesArrayHolder);
   }
 
   private getCategoriesDataFromJsonObject():void {
@@ -31,7 +143,17 @@ export class ItemComponent implements OnInit{
       Object.values(categoriesJsonData).map((value) => value)
     );
 
-    console.log(this.categories);
+    this.categories.splice(6,2);
+
   }
 
+  public removeImage(fileIndex: number){
+      this.filesArrayHolder.splice(fileIndex,1);
+  }
+
+  public fileDropped(files: FileHandle[]){
+    for(let file of files){
+      this.filesArrayHolder.push(file);
+    }
+  }
 }
