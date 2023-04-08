4acd37d4457ea68b9e027e89a5f63279d625ce39
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-04-08 14:34:56 +0000

    Add a second parameter

--- Backend/src/main/java/org/coders/youmarket/services/interfaces/ItemListingServiceInterface.java
+++ Backend/src/main/java/org/coders/youmarket/services/interfaces/ItemListingServiceInterface.java
@@ -2,8 +2,9 @@ package org.coders.youmarket.services.interfaces;
 
 import org.coders.youmarket.services.dtos.listing.item.ItemRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface ItemListingServiceInterface {
-    ResponseEntity<Object> createItemListing(ItemRequest itemRequest);
+    ResponseEntity<Object> createItemListing(ItemRequest itemRequest , MultipartFile[] files);
     ResponseEntity<Object> updateItemListing(ItemRequest itemRequest);
 }
