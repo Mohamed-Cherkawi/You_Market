db3156f05021a61919387f180950f0e404a21d1e
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-04-12 12:27:53 +0000

    Create the selling route

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import {HomeComponent} from "./modules/home/home.component";
 import {CreateListingComponent} from "./modules/create-listing/create-listing.component";
 import {ItemComponent} from "./modules/listings/item/item.component";
 import {CreateContainerComponent} from "./modules/create-container/create-container.component";
+import {SellingComponent} from "./modules/selling/selling.component";
 
 const routes: Routes = [
   { path : '', redirectTo: '/login', pathMatch: 'full' },
   { path : 'login' , component : LoginComponent , canActivate : [AuthenticationGuard] },
   { path : 'register' , component : RegisterComponent , canActivate : [AuthenticationGuard] },
   { path : 'home' , component : HomeComponent  },
+  { path : 'selling' , component : SellingComponent  },
   { path : 'create' , component : CreateContainerComponent , children : [
       { path : '' , component :  CreateListingComponent },
       { path : 'item' , component :  ItemComponent },
