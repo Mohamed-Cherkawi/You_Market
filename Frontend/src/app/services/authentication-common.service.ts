0a8e6b92c33412ebd37b3ee626f331c45081fae8
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-04-12 12:25:28 +0000

    Storing the userReference in localStorage

--- Frontend/src/app/services/authentication-common.service.ts
+++ Frontend/src/app/services/authentication-common.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import {Router} from "@angular/router";
+import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationCommonService {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private jwtHelper: JwtHelperService) { }
 
   public handleFormButton(): void {
     const button = document.querySelector("button");
@@ -20,10 +22,16 @@ export class AuthenticationCommonService {
   }
 
   public handleUserToken(token: string): void {
+    const decodedToken = this.jwtHelper.decodeToken(token);
+
     localStorage.setItem('token',token);
+    localStorage.setItem('userReference' , decodedToken.userReference);
+
     setTimeout(() => {
       localStorage.removeItem('token');
     }, 1000 * 60 * 60 * 24);
+
+    this.redirectToHome();
   }
 
   public redirectToHome(): void{
