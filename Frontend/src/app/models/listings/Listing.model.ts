e409dfda36a42bad5d033f933dd4ad546d8fc3f7
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-04-12 12:17:48 +0000

    modify and add attribute

--- Frontend/src/app/models/listings/Listing.model.ts
+++ Frontend/src/app/models/listings/Listing.model.ts
@@ -1,12 +1,14 @@
 import {Photo} from "../other/photo.model";
 import {ListingType} from "../../enums/listing-type.enum";
+import {FileHandle} from "../other/file-handle.model";
 
 export interface Listing {
   listingReference?: string | null;
-  ownerReference: string;
+  ownerReference: string | null;
   assets?: Photo[] | null;
   price: number;
   description: string;
   listingType: ListingType;
   updatedAt?: string | null;
+  fileHandling: FileHandle[];
 }
